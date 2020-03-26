26f59e4cf90edf74b4a06ea50a85b841cdf5c030
Nicolas Ries <nries1@gmail.com>
2020-03-26 12:13:48 -0400

    Add files via upload

---
+++ doTime.js
@@ -0,0 +1,30 @@
+let getRows = () => {
+  clear();
+  const weekDayRows = [...document.getElementsByTagName('TR')].filter(
+    _t =>
+      /Mon -|Tue -|Wed -|Thu -|Fri -/.test(_t.innerText) &&
+      !/BubbleSheet/.test(_t.innerText)
+  );
+  return weekDayRows;
+};
+
+let doTime = (start, lunchStart, lunchEnd, end) => {
+  const rows = getRows();
+  rows.forEach(_r => {
+    const cells = [..._r.cells];
+    cells.forEach(_c => {
+      if (!_c) return;
+      const field = _c.firstElementChild;
+      if (!field) return;
+      if (field.readonly) return;
+      const fName = field.name;
+      if (/in_/.test(fName)) field.value = start;
+      if (/lunchOut_/.test(fName)) field.value = lunchStart;
+      if (/lunchIn_/.test(fName)) field.value = lunchEnd;
+      if (/out_/.test(fName)) field.value = end;
+      if (/action_/.test(fName)) field.value = '1';
+      if (/code_/.test(fName)) field.value = 'WFH';
+      if (/absHours_/.test(fName)) field.value = '00:01';
+    });
+  });
+};
